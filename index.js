64a06a3fad41f206532e8ad1822358939894e8b3
jakedub <jacob.w.moore@gmail.com>
2017-08-17 15:32:20 -0400

    three things left

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const mustache = require ("mustache");
 const mustacheExpress = require('mustache-express');
 const bodyParser = require ("body-parser");
 
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 //mustache
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
@@ -19,42 +23,64 @@ app.use(session({
   cookie:{}
 }));
 
-//Redirect to login. // NOTE: Needs to have an if/else statement concerning login
-app.use((err, req, res, next)=>{
-  res.redirect("/login/");
+//Redirect to login if originalUrl is not "/login/"
+// app.use((req, res, next) => {
+//     if (req.originalUrl === '/') {
+//       next();
+//     }
+//   res.redirect("/login/")
+// });
+
+// NOTE: Not working
+app.use((err, req, res, next) => {
+  if (req.originalUrl === "/"){
+    next();
+  }
+  res.redirect('/login/');
 });
 
-app.get("/login/", function (req, res){
-  let html = '<form action="/" method="post">' +
-            '<h1>Login Page</h1>' +
-            '<p>Enter your username</p>' +
-            '<input type="text" name="username" placeholder="username" />'+
-            '<p>Enter your email</p>' +
-            '<input type="password" name="password" placeholder="password" />' +
-            '<button type="submit">Submit</button>' +
-       '</form>';
- res.send(html);
-})
+const list = [
+  {
+    username: "",
+    password: "",
+  }
+];
 
+const data = {
+  users:list
+};
+
+app.get('/login/', function (req, res) {
+  res.render("users", data);
+});
+
+// NOTE: Don't need this...I believe
 app.post("/", function(req, res){
   console.log(req.body);
-  let name = req.body.username;
-  let email = req.body.email;
-  let response = `
-  Your username is ${username}
-  Your password is ${password}`
-  res.send(response); //displays the email address
+  let username = req.body.username;
+  let password = req.body.password;
+  let response = `Here are your credentials for the site: <br/>
+  This is your username: ${username} <br/>
+  This is your password: ${password}`
+  res.send(response);
 });
 
+app.post("/", function (req, res) {
+  console.log(list);
+  list.push({username:req.body.username});
+  list.push({username:req.body.password});
+  res.redirect('/');
+})
 
 app.listen(3000, function () {
-  console.log('Starting to work');
+  console.log('Creating login page');
 })
+
+//Left To Do
+//1. User redirect if not logged in. Define log in?
+//2. authentication. Have the redirect with completed information. But need to authenticate.
+//3. Valid usernames/passwords kept as a data structure.
+
 //Using Express, Mustache, and express-session, create an app with a login page. When a user goes to /, and they are not logged in, redirect them to /login/. Upon entering a valid username and password, they should be authenticated and sent back to /. The root page should show that they are logged in and what username they are logged in as.
 
 // The valid usernames and passwords should be kept as a data structure in your application.
-
-//1. Install and set up Express, Mustache, and express-session. NOTE: Completed
-//2. Create the app to have a login page. So a form with username/password. Use "/login/"
-//3. User attempts to go to "/" and NOT logged in, redirect to /login/
-//4. User enters valid username/password they are authenticated and redirected to "/" and see username/password.
